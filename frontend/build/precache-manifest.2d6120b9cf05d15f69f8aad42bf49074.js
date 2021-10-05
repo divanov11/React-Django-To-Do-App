588d23274a20c365fbf9866b53802fcb76d67d42
divanov11 <ivanovsin11@gmail.com>
2021-10-05 11:38:15 -0400

    Updated build folder

---
+++ frontend/build/precache-manifest.2d6120b9cf05d15f69f8aad42bf49074.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cda70ca9839a5ccd9cc2711f181c1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c46b230cf97e86c8c533",
+    "url": "/static/css/main.bb56df75.chunk.css"
+  },
+  {
+    "revision": "1f911c779bc16728b3a3",
+    "url": "/static/js/2.2004f6e8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.2004f6e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c46b230cf97e86c8c533",
+    "url": "/static/js/main.8a6e935e.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
